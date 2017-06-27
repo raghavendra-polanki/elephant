ffdccd0208880ad21e0fa6b6690b01fbcdcf2c97
surenderthakran <surender.thakran89@gmail.com>
2017-06-27 15:22:54 +0530

    removed mongodb hardcoded options

--- application/lib/plugins/mongodb/index.js
+++ application/lib/plugins/mongodb/index.js
@@ -4,14 +4,9 @@ const Mongoose = require('mongoose');
 const Promise = require('bluebird');
 
 module.exports = (options, callback) => {
-  options = {
-    host: process.env.MONGO_PORT_27017_TCP_ADDR,
-    post: process.env.MONGO_PORT_27017_TCP_PORT,
-    db: 'elephant-dev',
-  };
   return new Promise((resolve, reject) => {
-    let dbConnection = Mongoose.createConnection('mongodb://' + options.host + ':' + options.port + '/' +
-        options.db);
+    let dbConnection = Mongoose.createConnection('mongodb://' + options.host +
+      ':' + options.port + '/' + options.db);
 
     dbConnection.on('connecting', () => {
       console.log('Connecting to mongo database');
