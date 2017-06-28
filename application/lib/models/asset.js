5567bb55f697cc7c353c314d647357e77b5786fe
surenderthakran <surender.thakran89@gmail.com>
2017-06-28 17:31:50 +0530

    updated asset model with custom options

--- application/lib/models/asset.js
+++ application/lib/models/asset.js
@@ -3,5 +3,51 @@
 const Mongoose = require('mongoose');
 
 module.exports = new Mongoose.Schema({
-  asset_id: 'string',
+  asset_id: String,
+  urls: [
+    {
+      url: String,
+      src: {
+        type: String,
+        enum: ['YOUTUBE', 'VIMEO'],
+      },
+    },
+  ],
+  title: [
+    {
+      val: String,
+      lang: {
+        type: String,
+        enum: ['ENGLISH', 'HINDI'],
+      },
+    },
+  ],
+  desc: [
+    {
+      val: String,
+      lang: {
+        type: String,
+        enum: ['ENGLISH', 'HINDI'],
+      },
+    },
+  ],
+  length: Number,
+  lang: {
+    val: String,
+    enum: ['ENGLISH', 'HINDI'],
+  },
+  rating: {
+    val: String,
+    enum: ['U', 'UA', 'A'],
+  },
+  curator_id: String,
+  curated_on: Number,
+  verifier_id: String,
+  verified_on: Number,
+  created_at: Number,
+  updated_at: Number,
+}, {
+  bufferCommands: false, // disable command buffering.
+  collection: 'assets',
+  strict: 'throw',
 });
