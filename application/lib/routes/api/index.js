94d4021fd5b2e17fc492a6eac45e4cd14b4189f8
surenderthakran <surender.thakran89@gmail.com>
2017-06-29 17:34:01 +0530

    test api updated to generate unique asset ids

--- application/lib/routes/api/index.js
+++ application/lib/routes/api/index.js
@@ -7,9 +7,21 @@ const Express = require('express');
 const Router = Express.Router();
 
 Router.get('/', function(req, res) {
-	console.log(new Date());
-	console.log($.utils.dateTime.currentYearMonth());
-	res.send('Hello API');
+	// $.model.Test.create({
+	// 	id: 'abc',
+	// }, (err, doc) => {
+	// 	console.error(err);
+	// 	console.log(doc);
+	// });
+
+	$.mongodb.db.collection('asset_counter').findAndModify(
+			{prefix: $.utils.dateTime.currentYearMonthDate()}, [],
+			{$inc: {count: 1}},
+			{upsert: true, new: true}, (err, doc) => {
+				console.log(err);
+				console.log(doc);
+				res.send(200, doc.value.count);
+			});
 });
 
 module.exports = Router;
