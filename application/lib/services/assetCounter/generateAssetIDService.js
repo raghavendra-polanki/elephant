fa77484562ef3807f451fe79b78762ddd7531f08
surenderthakran <surender.thakran89@gmail.com>
2017-07-04 23:48:48 +0530

    updated service to generate new asset id

--- application/lib/services/assetCounter/generateAssetIDService.js
+++ application/lib/services/assetCounter/generateAssetIDService.js
@@ -6,9 +6,21 @@ module.exports = {
   pattern: {
     cmd: 'generate_asset_id',
   },
-  description: 'test',
+  description: 'Service to generate a new asset ID',
   handler: (message, callback) => {
     console.log('inside generate_asset_id');
-    callback();
+
+    return new Promise((resolve, reject) => {
+      $.mongodb.db.collection('asset_counter').findAndModify(
+        {prefix: $.utils.dateTime.currentYearMonthDate()}, [],
+        {$inc: {count: 1}},
+        {upsert: true, new: true}, (err, doc) => {
+          if (err != null) {
+            console.error(err);
+            return reject(err);
+          }
+          return resolve(doc.value.count);
+        });
+    });
   },
 };
