38a99a50f1d441a69ccf963c7964f00640a99027
surenderthakran <surender.thakran89@gmail.com>
2017-07-09 20:59:38 +0530

    created generate_category_id service

---
+++ application/lib/services/IDGenerators/generateCategoryIDService.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const $ = require(__base + 'lib');
+
+module.exports = {
+  pattern: {
+    cmd: 'generate_category_id',
+  },
+  description: 'Service to generate a new category ID',
+  handler: (message, callback) => {
+    console.log('inside generate_category_id service');
+
+    $.mongodb.db.collection('category_counter').findAndModify(
+      {prefix: $.utils.dateTime.currentYearMonthDate()}, [],
+      {$inc: {count: 1}},
+      {upsert: true, new: true}, (err, doc) => {
+        if (err != null) {
+          console.error(err);
+          return callback(err, null);
+        }
+        return callback(null, {id: doc.value.count});
+      });
+  },
+};
