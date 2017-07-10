419b512f2286b84959eb467ff7617dcf8fca3b9f
surenderthakran <surender.thakran89@gmail.com>
2017-07-10 19:02:42 +0530

    updated api/insert_category endpoint

--- application/lib/routes/api/handlers/insertCategory.js
+++ application/lib/routes/api/handlers/insertCategory.js
@@ -2,18 +2,34 @@
 
 const $ = require(__base + 'lib');
 
+const replyWithValidationError = (res, error) => {
+  if (error.name == 'ValidationError') {
+    let valErrors = [];
+    for (let field in error.errors) {
+      valErrors.push(error.errors[field].message)
+    }
+    res.status(500).json({err: valErrors});
+  } else {
+    res.status(500).json({err: 'error while validating schema'});
+  }
+};
+
 module.exports = function(req, res, next) {
   console.log('inside /api/insert_category');
 
   console.log(req.body);
 
   let categoryData = new $.model.Category(req.body);
+  console.log(categoryData);
 
   categoryData.validate((err) => {
     if (err) {
       console.error(err);
+      replyWithValidationError(res, err);
+    } else {
+      console.log('valid');
+      res.status(200).json({'a': 1});
     }
-    res.json({'a': 1});
   });
 
   // $.act({
