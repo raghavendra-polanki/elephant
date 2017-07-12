b7d6ccf4eaef81c15d7aa652f1bee7a685abbc1e
surenderthakran <surender.thakran89@gmail.com>
2017-07-13 00:53:15 +0530

    added new validation utility

---
+++ application/lib/utils/validationUtility.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const $ = require(__base + 'lib');
+
+let externals = {};
+
+const compileValidationErrors = (error) => {
+  if (error.name == 'ValidationError') {
+    let valErrors = [];
+    for (let field in error.errors) {
+      valErrors.push(error.errors[field].message);
+    }
+    return valErrors;
+  } else {
+    return 'error while validating schema';
+  }
+};
+
+externals.validateInstanceSchema = (instance) => {
+  console.log($);
+  return new $.promise((resolve, reject)=> {
+    instance.validate((err) => {
+      if (err) {
+        console.error(err);
+        reject(compileValidationErrors(err));
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+module.exports = externals;
