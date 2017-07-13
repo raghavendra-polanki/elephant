d4323ab34001935d98d02a6265438c55a520f1d0
surenderthakran <surender.thakran89@gmail.com>
2017-07-13 23:10:41 +0530

    moved schema names validation to validation utility

--- application/lib/models/category.js
+++ application/lib/models/category.js
@@ -10,50 +10,7 @@ module.exports = new Mongoose.Schema({
                        // dangling ids in database in case of validation fails.
   name: {
     type: Mongoose.Schema.Types.Mixed,
-    validate: [
-      {
-        validator: (data) => {
-          if (!Object.keys(data).length) {
-            return false;
-          }
-          return true;
-        },
-        message: 'Category must have atleast one valid name.',
-      },
-      {
-        validator: function(data) {
-          let hasUnsupportedLanguage = false;
-          Object.keys(data).forEach((lang) => {
-            if ($.constants.supportedLanguages
-                .indexOf(lang.toLowerCase()) === -1) {
-              hasUnsupportedLanguage = true;
-            }
-          });
-
-          if (hasUnsupportedLanguage) {
-            return false;
-          }
-          return true;
-        },
-        message: 'Category names must be in one of the supported language.',
-      },
-      {
-        validator: (data) => {
-          let hasDefaultLanguage = false;
-          Object.keys(data).forEach((lang) => {
-            if (lang.toLowerCase() === $.constants.defaultLanguage) {
-              hasDefaultLanguage = true;
-            }
-          });
-
-          if (!hasDefaultLanguage) {
-            return false;
-          }
-          return true;
-        },
-        message: 'Category must have a name in english.',
-      },
-    ],
+    validate: $.utils.validation.validateSchemaNames,
   },
   children: [ // these are category_ids.
     String,
--- application/lib/utils/validationUtility.js
+++ application/lib/utils/validationUtility.js
@@ -31,4 +31,49 @@ externals.validateInstanceSchema = (instance) => {
   });
 };
 
+externals.validateSchemaNames = [
+  {
+    validator: (data) => {
+      if (!Object.keys(data).length) {
+        return false;
+      }
+      return true;
+    },
+    message: 'Category must have atleast one valid name.',
+  },
+  {
+    validator: function(data) {
+      let hasUnsupportedLanguage = false;
+      Object.keys(data).forEach((lang) => {
+        if ($.constants.supportedLanguages
+            .indexOf(lang.toLowerCase()) === -1) {
+          hasUnsupportedLanguage = true;
+        }
+      });
+
+      if (hasUnsupportedLanguage) {
+        return false;
+      }
+      return true;
+    },
+    message: 'Category names must be in one of the supported language.',
+  },
+  {
+    validator: (data) => {
+      let hasDefaultLanguage = false;
+      Object.keys(data).forEach((lang) => {
+        if (lang.toLowerCase() === $.constants.defaultLanguage) {
+          hasDefaultLanguage = true;
+        }
+      });
+
+      if (!hasDefaultLanguage) {
+        return false;
+      }
+      return true;
+    },
+    message: 'Category must have a name in english.',
+  },
+];
+
 module.exports = externals;
