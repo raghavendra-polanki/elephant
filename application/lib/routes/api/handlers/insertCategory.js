fe1c92ea7843630602a473cd58fb9a89b724c13a
surenderthakran <surender.thakran89@gmail.com>
2017-07-13 23:35:41 +0530

    updated /api/insert_category endpoint to save category document to mongo

--- application/lib/routes/api/handlers/insertCategory.js
+++ application/lib/routes/api/handlers/insertCategory.js
@@ -5,31 +5,26 @@ const $ = require(__base + 'lib');
 module.exports = function(req, res, next) {
   console.log('inside /api/insert_category');
 
-  console.log(req.body);
-
   let categoryData = new $.model.Category(req.body);
-  console.log(categoryData);
 
   $.utils.validation.validateInstanceSchema(categoryData)
   .then(() => {
     console.log('valid');
-    res.status(200).json({'a': 1});
+    return $.act({
+      cmd: 'generate_category_id',
+    });
+  })
+  .then((result) => {
+    console.log('generate_category_id returned');
+    console.log(result);
+    return categoryData.save();
+  })
+  .then(() => {
+    console.log('inserted');
+    res.status(200).json({status: 0});
   })
   .catch((err) => {
+    console.error('in catch()');
     res.status(500).json({err: err});
   });
-
-  // $.act({
-  //   cmd: 'generate_category_id',
-  // })
-  // .then((result) => {
-  //   console.log('generate_category_id returned');
-  //   console.log(result);
-  //   res.status(200).send(result.id.toString());
-  // })
-  // .catch((err) => {
-  //   console.error('in catch()');
-  //   console.error(err);
-  //   res.status(500).send('something went wrong');
-  // });
 };
