130c933ba20cbd3da221ed5e0477510e7daffe7f
surenderthakran <surender.thakran89@gmail.com>
2017-07-15 22:05:41 +0530

    not using middleware approach to lowercase request parameters

--- application/lib/routes/api/index.js
+++ application/lib/routes/api/index.js
@@ -10,13 +10,6 @@ const Router = Express.Router();
 Router.use(BodyParser.json());
 Router.use(BodyParser.urlencoded({extended: true}));
 
-// middleware to change all fields in input to lowercase.
-Router.use((req, res, next) => {
-  $.log.Info('inside middleware');
-  $.log.Info(req.body);
-  next();
-});
-
 // Level 1 APIs.
 
 Router.post('/api/insert_category',
