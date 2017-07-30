cec9966d9b1be188499b9a85dfa2aa6cf72ca192
surenderthakran <surender.thakran89@gmail.com>
2017-07-30 23:24:33 +0530

    updated api/video/insert to validate request data

--- application/lib/routes/api/handlers/video/insert.js
+++ application/lib/routes/api/handlers/video/insert.js
@@ -2,6 +2,28 @@
 
 const $ = require(__base + 'lib');
 
+const processRequest = async (req, res, next) => {
+  let videoData;
+  try {
+    videoData = new $.model.Video(req.body);
+
+    let currentTimestamp = Date.now();
+    videoData.set({
+      created_at: currentTimestamp,
+      updated_at: currentTimestamp,
+      curator_id: 'initial', // TODO(surenderthakran): update when user addition
+                             // is enabled.
+    });
+
+    // validate category schema.
+    await $.utils.validation.validateInstanceSchema(videoData);
+  } catch (err) {
+    $.log.Error(err);
+    res.status(500).json({status: 'INVALID_ARGUMENT', error: err});
+    return;
+  }
+};
+
 module.exports = function(req, res, next) {
   processRequest(req, res, next)
   .then((data) => {
